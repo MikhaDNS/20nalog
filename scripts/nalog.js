9614e0dbd263177dfe6e0322ae18aba31bd648a8
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-25 13:23:56 +0300

    Update nalog.js

--- scripts/nalog.js
+++ scripts/nalog.js
@@ -1,3 +1,8 @@
+function numberWithSpaces(x) {
+  var parts = x.toString().split(".");
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  return parts.join(".");
+}
 function result() {
   let summ = document.getElementById("summ").value;
    
@@ -5,7 +10,7 @@ if (summ.match(/^[-\+]?\d+/) == null || summ == 0){
   alert('Проверьте корректность ввода данных!');
 }else{
   let rezmath=(((summ/120)*100)*0.2).toFixed(3);
-  document.getElementById("mainlut").textContent="Ваш налог составил: "+rezmath.toLocaleString();
+  document.getElementById("mainlut").textContent="Ваш налог составил: "+numberWithSpaces(rezmath)+" руб."
 
 }
 }
