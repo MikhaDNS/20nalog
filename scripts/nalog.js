c2a355f30361d46350bb6a2af1b83e62bc1031f3
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-25 13:03:09 +0300

    Update nalog.js

--- scripts/nalog.js
+++ scripts/nalog.js
@@ -7,7 +7,7 @@ if (summ.match(/^[-\+]?\d+/) == null || summ == 0){
   alert('Проверьте корректность ввода данных!');
 }else{
   let rezmath=(((summ/120)*100)*0.2).toFixed(3);
-  document.getElementById("mainlut").textContent="Ваш налог составил: "+rezmath+" руб.";
+  document.getElementById("mainlut").textContent="Ваш налог составил: "+rezmath.toLocaleString()+" руб.";
 
 }
 }
